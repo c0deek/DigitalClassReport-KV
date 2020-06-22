03796af1ae78e3008b0e3ab05adad4a749a38ed2
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2020-06-23 00:48:30 +0530

    model added

--- Report/models.py
+++ Report/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 # Create your models here.
 DESIGNATIONS = [
-    ("PRT", "prt"),
+    ("prt", "PRT"),
 ]
 
-class Teachers(models.Model):
+class Teacher(models.Model):
     name = models.CharField(max_length = 30, unique=True)
     designation = models.CharField(max_length=3, choices=DESIGNATIONS)
 
+    def __str__(self):
+        return self.name
+
 class Record(models.Model):
-    class_name = models.IntegerField(default = 1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    Class = models.IntegerField(default = 1, validators=[MinValueValidator(1), MaxValueValidator(12)])
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
     section = models.CharField(max_length = 3)
     subject = models.CharField(max_length = 20)
     no_students = models.IntegerField(default = 0)
     platform = models.CharField(max_length = 20)
+    date = models.DateField(default = timezone.now)
+
+    def __str__(self):
+        return f"{self.date}: {self.teacher} {self.subject} {self.Class}"
